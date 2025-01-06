1a9c27f2457f380776b37c0c03e5de627d1296d4
Rohit Nigam <rohitnigam354@gmail.com>
2025-01-06 22:43:54 +0530

    Time: 63 ms (7.99%), Space: 18.3 MB (6.2%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -4,15 +4,16 @@ class Solution:
         left = 0
         for right in range(1,len(nums)):
             print(left,right)
-            if nums[left] == nums[right]:
-                count += 1
-            elif nums[left] != nums[right] and count > 0:
+            if nums[left] != nums[right] and count > 0:
                 count = 0
                 left += 2
                 nums[left] = nums[right]
             elif nums[left]!=nums[right] and count==0:
                 left+=1
                 nums[left] = nums[right]
+            elif nums[left] == nums[right]:
+                count += 1
+                nums[left+1]=nums[left]
         if count>0:
             left+=1
             nums[left]=nums[right]
