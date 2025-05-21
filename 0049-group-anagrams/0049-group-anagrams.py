97210f8170f3af46374eaea83bdf997d05d69883
Rohit Nigam <rohitnigam354@gmail.com>
2025-05-21 15:45:28 +0530

    Delete 0049-group-anagrams directory

--- 0049-group-anagrams/0049-group-anagrams.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result=defaultdict(list)
-        for i in strs:
-            result["".join(sorted(i))].append(i)
-        return list(result.values())
